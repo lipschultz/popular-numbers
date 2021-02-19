cd6671d5adcbfcdd52461413ef01db283d135824
Michael Lipschultz <michael.lipschultz@gmail.com>
2021-02-18 19:03:46 -0500

    Remove dead code

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import sqlite3
-import sqlite3
-import time
-from collections import namedtuple
-from multiprocessing import freeze_support, Process, Queue, cpu_count
-from typing import List
+from multiprocessing import freeze_support
 
-from tqdm import tqdm
-
-from popularity import load
+from popularity.compute_popularity import single_process
 from popularity.number_generator import NumberGenerator
 
 
 def main():
     youtube_file = 'data/youtube.json'
 
-    db_url = r'results-negative.db'
-    run0 = NumberGenerator(-10_000, -9_999, 2,  # Real
-                           # -10_000, 10_000, 2,  # Imaginary
-                           db_url=db_url)
+    db_url = r'results.db'
+    reals_run = NumberGenerator(
+        -10_000, -9_999, 2,  # Real
+        0, 0, 1,  # Imaginary
+        db_url=db_url
+    )
 
-    # db_url = r'D:\results-positive.db'
-    # run0 = NumberGenerator(9_998, 10_000, 2,  # Real
-    #                        -10_000, 10_000, 2,  # Imaginary
-    #                        db_url=db_url)
+    imaginary_run = NumberGenerator(
+        0, 0, 1,  # Real
+        -10_000, -9_999, 2,  # Imaginary
+        db_url=db_url
+    )
+
+    complex_run = NumberGenerator(-10_000, 10_000, 2, db_url=db_url)
+
+    run_pie = NumberGenerator(1.7, 4.2, 5, 0, 0, 0, skip=reals_run)
 
     conn = sqlite3.connect(db_url)
     with conn:
@@ -35,13 +36,9 @@ def main():
             PRIMARY KEY (real, imag, link)
         );''')
 
-
-    # run0 = NumberGenerator(-10_000, 10_000, 2, db_url=db_url)
-    # run_pie = NumberGenerator(1.7, 4.2, 5, 0, 0, 0, skip=run0)
-
-    results = single_process(youtube_file, run0, conn)
+    results = single_process(youtube_file, reals_run, conn)
     conn.close()
-    # results = mp_queue(youtube_file, run0, db_url)
+
     return results
 
 
